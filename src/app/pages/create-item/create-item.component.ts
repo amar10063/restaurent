9f669e94000ee98866927aaef713349e100d97dc
amar10063 <amar10063@yahoo.co.in>
2020-02-26 16:28:07 +0530

    delete added

--- src/app/pages/create-item/create-item.component.ts
+++ src/app/pages/create-item/create-item.component.ts
@@ -13,6 +13,7 @@ export class CreateItemComponent implements OnInit {
   massage: string;
   item: Items;
   aa: [];
+  updateFlag: number;
   constructor(private pagesDataService: PagesDataService) { }
   public show: boolean = true;
   public hide: boolean = false;
@@ -30,14 +31,21 @@ export class CreateItemComponent implements OnInit {
       'itemCode': new FormControl(null, Validators.required),
       'Price': new FormControl(null, Validators.required),
       'Description': new FormControl(null, Validators.required),
-      'Flag': new FormControl('5'),
+      // 'Flag': new FormControl('5'),
     });
     this.allItem();
   }
   onSubmit() {
     const item = this.createItemForm.value;
     console.log(this.createItemForm.value);
-    this.createItem(item);
+    alert(item.itemCode);
+    if (item.itemCode == '') {
+      item.Flag = '5';
+      this.createItem(item);
+    } else {
+      item.Flag = '2';
+      this.updateItem(item);
+    }
   }
   createItem(item: any) {
     this.pagesDataService.createItem(item).subscribe(
@@ -59,6 +67,41 @@ export class CreateItemComponent implements OnInit {
       this.item = data;
       this.aa = this.item.dataList;
       console.log(this.aa);
+    });
+  }
+  bindItem(item: any) {
+    console.log(item);
+    //  item.Flag = '2';
+    this.createItemForm.controls.itemName.setValue(item.ItemName);
+    this.createItemForm.controls.itemCode.setValue(item.ItemCode);
+    this.createItemForm.controls.Price.setValue(item.Price);
+    this.createItemForm.controls.Description.setValue(item.Description);
+    // this.registerForm.controls.Password.setValue(employee.Flag);
+    this.updateFlag = 1;
+    this.newForm();
+    console.log(item);
+  }
+  updateItem(item: any) {
+    this.pagesDataService.updateItem(item).subscribe(
+      (data) => {
+        this.massage = 'Item Created';
+        alert(this.massage);
+        this.item = data;
+        console.log(this.item);
+        alert(this.item.Status);
+        this.aa = this.item.dataList;
+        console.log(this.aa);
+        this.createItemForm.reset();
+        this.backForm();
+      }
+    )
+  }
+  deleteItem(item: any) {
+    item.Flag = '3';
+    this.pagesDataService.deleteItem(item).subscribe(() => {
+      this.massage = 'Record deleted succesfully';
+      alert(this.massage);
+      this.allItem();
     });
   }
 }
